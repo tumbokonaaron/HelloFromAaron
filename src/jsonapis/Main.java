a6b3e4dbfc62e54fe98c343489b3152ef9813939
bdayley <col04002@byui.edu>
2021-05-22 14:20:28 -0600

    Hello Aaron, my name is Brianna

--- src/jsonapis/Main.java
+++ src/jsonapis/Main.java
@@ -3,6 +3,8 @@ package jsonapis;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Aaron");
+        System.out.println("Hello Aaron, my name is Brianna");
     }
 }
